3545780e2b42d1bdc05b1c616286edd630ad2219
Ankit Jha <ankitjha14@gmail.com>
2018-03-11 16:54:16 +0530

    Add service

--- src/main/kotlin/com/example/demo/Controller.kt
+++ src/main/kotlin/com/example/demo/Controller.kt
@@ -1,13 +1,17 @@
 package com.example.demo
 
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.PathVariable
 
 @RestController
 class Controller {
 
+    @Autowired
+    var service: Service = Service()
+
     @RequestMapping("/{name}")
-    fun index(@PathVariable name: String) = "Hello $name!!"
+    fun index(@PathVariable name: String) = service.name(name)
 
 }
