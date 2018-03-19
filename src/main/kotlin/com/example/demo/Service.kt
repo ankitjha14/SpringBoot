533ac615bd1f1ceb1d52057c24ce11c311de541b
Ankit Jha <ankitjha14@gmail.com>
2018-03-19 11:53:22 +0530

    added modified table example beauty of kotlin

--- src/main/kotlin/com/example/demo/Service.kt
+++ src/main/kotlin/com/example/demo/Service.kt
@@ -4,14 +4,8 @@ import org.springframework.stereotype.Service
 
 @Service
 class Service {
+
     fun name(name: String) = "Hello $name!!"
-    fun table(no: Int): IntArray {
-        val arr: IntArray = kotlin.IntArray(10)
+    fun table(no: Int) = IntArray(10) {i -> (i+1)*no}
 
-        for (i: Int in 1..10) {
-            arr.set(i - 1, i * no)
-        }
-
-        return arr
-    }
 }
